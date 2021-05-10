28bb761a68694ffb75a9fd7937b832f2ad380d4f
Chrys <t@theadmin.co.uk>
2021-05-10 23:12:22 +0100

    Initial Page layouting

--- AndroidApp/app/src/main/java/uk/co/nhickling/imriescar/MainActivity.java
+++ AndroidApp/app/src/main/java/uk/co/nhickling/imriescar/MainActivity.java
@@ -3,6 +3,8 @@ package uk.co.nhickling.imriescar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnclick_ToggleNerdStats(View view){
+        TableLayout layout = (TableLayout)findViewById(R.id.block_NerdStatsDetail);
+        layout.setVisibility(layout.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
     }
 }
